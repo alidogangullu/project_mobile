77187b8a2c58842a7bd61c83ffefe63ee92eb161
ADG <alidogangullu@outlook.com>
2022-11-12 20:32:20 +0300

    changed authentication ui and deleted unnecessary pages. without firebase auth connection

--- lib/Authentication/loginPage.dart
+++ lib/Authentication/loginPage.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:project_mobile/Authentication/forgotPassword.dart';
 import 'package:project_mobile/Authentication/sign_upPage.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final TextEditingController phoneController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -11,222 +18,47 @@ class LoginPage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          InputField(const Icon(Icons.person_outline), "E-Mail"),
-          PasswordField(),
-          LoginButton(),
-          AuthText('Forgot Password?', ForgotPasswordPage()),
-          AuthText('Not a member? Sign up now!', SignUpPage()),
-        ],
-      ),
-    );
-  }
-}
-
-Widget LoginButton() {
-  return Padding(
-    padding: const EdgeInsets.all(10.0),
-    child: ElevatedButton(
-      style: ElevatedButton.styleFrom(
-          //primary: Colors.green,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
+          PhoneField(phoneController: phoneController),
+          const SizedBox(
+            height: 20,
           ),
-          minimumSize: Size(150, 55)),
-      onPressed: () => {},
-      child: const Text(
-        "Sign In",
-        style: TextStyle(
-          fontSize: 20,
-        ),
-      ),
-    ),
-  );
-}
-
-class SignUp extends StatelessWidget {
-  const SignUp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 15, 15, 15),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-            primary: Colors.redAccent,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-            minimumSize: Size(150, 55)),
-        onPressed: () => {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SignUpPage()),
-          )
-        },
-        child: const Text(
-          "Sign Up",
-          style: TextStyle(
-            fontSize: 20,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-Widget InputField(Icon prefixIcon, String hintText) {
-  return Padding(
-    padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-    child: Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(30),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black,
-            blurRadius: 25,
-            offset: Offset(0, 5),
-            spreadRadius: -25,
-          ),
-        ],
-      ),
-      child: TextField(
-        style: const TextStyle(
-          fontSize: 17,
-        ),
-        decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: const TextStyle(
-            color: Colors.grey,
-          ),
-          fillColor: Colors.white,
-          filled: true,
-          prefixIcon: prefixIcon,
-          prefixIconConstraints: const BoxConstraints(
-            minWidth: 75,
-          ),
-          enabledBorder: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(50),
-            ),
-            borderSide: BorderSide(color: Colors.grey),
-          ),
-          focusedBorder: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(50),
-            ),
-            borderSide: BorderSide(color: Colors.blue),
-          ),
-        ),
-      ),
-    ),
-  );
-}
-
-class PasswordField extends StatefulWidget {
-  const PasswordField({Key? key}) : super(key: key);
-
-  @override
-  State<PasswordField> createState() => _PasswordFieldState();
-}
-
-class _PasswordFieldState extends State<PasswordField> {
-  late bool _passwordVisible;
-  @override
-  void initState() {
-    _passwordVisible = false;
-  }
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-      child: Container(
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(
-            Radius.circular(30),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black,
-              blurRadius: 25,
-              offset: Offset(0, 5),
-              spreadRadius: -25,
-            ),
-          ],
-        ),
-        child: TextField(
-          obscureText: _passwordVisible,
-          style: const TextStyle(
-            fontSize: 17,
-          ),
-          decoration: InputDecoration(
-            suffixIcon: IconButton(
-              icon: Icon(
-                _passwordVisible
-                    ? Icons.visibility
-                    : Icons.visibility_off,
-                color: Colors.grey,
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  //primary: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  minimumSize: Size(150, 55)),
+              onPressed: () => {},
+              child: const Text(
+                "Sign In",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
-              onPressed: () {
-                setState(() {
-                  _passwordVisible = !_passwordVisible;
-                });
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpPage()),
+                );
               },
-            ),
-            hintText: "Password",
-            hintStyle: const TextStyle(
-              color: Colors.grey,
-            ),
-            fillColor: Colors.white,
-            filled: true,
-            prefixIcon: Icon(Icons.lock_outline),
-            prefixIconConstraints: const BoxConstraints(
-              minWidth: 75,
-            ),
-            enabledBorder: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(50),
+              child: const Text(
+                'Not a member? Sign up now!',
+                style: TextStyle(fontSize: 14, color: Colors.grey),
               ),
-              borderSide: BorderSide(color: Colors.grey),
-            ),
-            focusedBorder: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(50),
-              ),
-              borderSide: BorderSide(color: Colors.blue),
             ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-
-class AuthText extends StatefulWidget {
-  final String text;
-  final Widget route;
-  AuthText(this.text, this.route);
-
-  @override
-  State<AuthText> createState() => _AuthTextState();
-}
-
-class _AuthTextState extends State<AuthText> {
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => widget.route),
-          );
-        },
-        child: Text(
-          widget.text,
-          style: TextStyle(fontSize: 14, color: Colors.grey),
-        ),
+        ],
       ),
     );
   }
--- lib/Authentication/sign_upPage.dart
+++ lib/Authentication/sign_upPage.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:project_mobile/Authentication/loginPage.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController surnameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,14 +20,29 @@ class SignUpPage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          _inputField(const Icon(Icons.person_outlined), "E-Mail", false),
-          _inputField(const Icon(Icons.lock_outline), "Password", true),
-          _inputField(const Icon(Icons.lock_outline), "Password Again", true),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SignUp(),
-            ],
+          PhoneField(phoneController: phoneController),
+          InputField(const Icon(Icons.person_outlined), "Name", nameController),
+          InputField(const Icon(Icons.person_outlined), "Surname", surnameController),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 15, 15, 15),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    minimumSize: Size(150, 55)
+                ),
+                onPressed: (){},
+                child: const Text(
+                  "Sign Up",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
@@ -25,83 +50,119 @@ class SignUpPage extends StatelessWidget {
   }
 }
 
-class SignUp extends StatelessWidget {
-  const SignUp({Key? key}) : super(key: key);
+class PhoneField extends StatelessWidget {
+  const PhoneField({
+    Key? key,
+    required this.phoneController,
+  }) : super(key: key);
+
+  final TextEditingController phoneController;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 15, 15, 15),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-            primary: Colors.redAccent,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-            minimumSize: Size(150, 55)
-        ),
-        onPressed: () => {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => LoginPage()),
-          )
-        },
-        child: const Text(
-          "Sign Up",
-          style: TextStyle(
-            fontSize: 20,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-Widget _inputField(Icon prefixIcon, String hintText, bool isPassword) {
-  return Padding(
-    padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-    child: Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(30),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black,
-            blurRadius: 25,
-            offset: Offset(0, 5),
-            spreadRadius: -25,
-          ),
-        ],
-      ),
-      child: TextField(
-        obscureText: isPassword,
-        style: const TextStyle(
-          fontSize: 17,
-        ),
-        decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: const TextStyle(
+      padding: const EdgeInsets.fromLTRB(50, 10, 30, 10),
+      child: InternationalPhoneNumberInput(
+        textAlign: TextAlign.center,
+        searchBoxDecoration: const InputDecoration(
+          hintText: "Country Code",
+          hintStyle: TextStyle(
             color: Colors.grey,
           ),
           fillColor: Colors.white,
           filled: true,
-          prefixIcon: prefixIcon,
-          prefixIconConstraints: const BoxConstraints(
+          prefixIcon: Icon(Icons.add_outlined),
+          prefixIconConstraints: BoxConstraints(
             minWidth: 75,
           ),
-          enabledBorder: const OutlineInputBorder(
+          enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.all(
               Radius.circular(50),
             ),
             borderSide: BorderSide(color: Colors.grey),
           ),
-          focusedBorder: const OutlineInputBorder(
+          focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.all(
               Radius.circular(50),
             ),
             borderSide: BorderSide(color: Colors.blue),
           ),
+        ),
+        inputDecoration: const InputDecoration(
+          hintText: "5xx xxx xxxx",
+          hintStyle: TextStyle(
+            color: Colors.grey,
+          ),
+          fillColor: Colors.white,
+          filled: true,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(50),
+            ),
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(50),
+            ),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+        ),
+        onInputChanged: (PhoneNumber number) {
+          print(number.phoneNumber);
+        },
+        onInputValidated: (bool value) {
+          print(value);
+        },
+        selectorConfig: const SelectorConfig(
+          selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+          trailingSpace: false,
+        ),
+        ignoreBlank: false,
+        autoValidateMode: AutovalidateMode.disabled,
+        initialValue: PhoneNumber(isoCode: 'TR'),
+        textFieldController: phoneController,
+        formatInput: false,
+        keyboardType: const TextInputType.numberWithOptions(
+            signed: true, decimal: true),
+        onSaved: (PhoneNumber number) {
+          print('On Saved: $number');
+        },
+      ),
+    );
+  }
+}
+
+Widget InputField(Icon prefixIcon, String hintText, TextEditingController controller) {
+  return Padding(
+    padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+    child: TextField(
+      controller: controller,
+      style: const TextStyle(
+        fontSize: 17,
+      ),
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          color: Colors.grey,
+        ),
+        fillColor: Colors.white,
+        filled: true,
+        prefixIcon: prefixIcon,
+        prefixIconConstraints: const BoxConstraints(
+          minWidth: 75,
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(50),
+          ),
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(50),
+          ),
+          borderSide: BorderSide(color: Colors.blue),
         ),
       ),
     ),
