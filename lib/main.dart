efe77ca7c0609a8b682fa7b7433b654a56bec627
murselytm <61565081+murselytm@users.noreply.github.com>
2022-11-12 21:20:07 +0300

    Demo demo yaptım hep

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo Demo Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
