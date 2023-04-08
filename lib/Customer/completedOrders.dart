3da4aa383f0b03887a8244d9c60bc69914a8d4ef
Ali Doğan Güllü <alidogangullu@outlook.com>
2023-04-09 01:07:14 +0300

    CompletedOrders tasarımı düzenlendi.
    OrderDetailsPage için navigation eklendi.

--- lib/Customer/completedOrders.dart
+++ lib/Customer/completedOrders.dart
@@ -28,7 +28,7 @@ class CompletedOrdersScreen extends StatelessWidget {
             return Text('Error: ${snapshot.error}');
           }
           if (!snapshot.hasData) {
-            return const SizedBox();
+            return const Center(child: CircularProgressIndicator(),);
           }
           return ListView.builder(
             itemCount: snapshot.data!.docs.length,
@@ -43,44 +43,67 @@ class CompletedOrdersScreen extends StatelessWidget {
                     return Text('Error: ${snapshot.error}');
                   }
                   if (!snapshot.hasData) {
-                    return const Center(child: CircularProgressIndicator(),);
+                    return const SizedBox();
                   }
+
                   final restaurantName = snapshot.data!.get('name') as String;
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        restaurantName,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
+                  final timestamp = order["timestamp"];
+                  final dateTime = timestamp.toDate().toLocal();
+                  final formattedDate = "${dateTime.day.toString().padLeft(2, '0')}.${dateTime.month.toString().padLeft(2, '0')}.${dateTime.year.toString()} ${dateTime.hour.toString().padLeft(2, '0')}.${dateTime.minute.toString().padLeft(2, '0')}";
+
+                  return Card(
+                    child: ListTile(
+                      title: Text(restaurantName),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: items.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                final itemRef = items[index]['itemRef'] as DocumentReference;
+                                return FutureBuilder<DocumentSnapshot>(
+                                  future: itemRef.get(),
+                                  builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                                    if (snapshot.hasError) {
+                                      return Text('Error: ${snapshot.error}');
+                                    }
+                                    if (!snapshot.hasData) {
+                                      return const SizedBox();
+                                    }
+                                    final itemName = snapshot.data!.get('name') as String;
+                                    return Text(
+                                        '- $itemName'
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                          Text(formattedDate),
+                        ],
+                      ),
+                      trailing: SizedBox(
+                        width: 100,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text("250\$"), //test için eklendi databaseden çekilecek
+                            IconButton(icon: const Icon(Icons.arrow_forward), onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => OrderDetailsPage()
+                                ),
+                              );
+                            },),
+                          ],
                         ),
                       ),
-                      ListView.builder(
-                        shrinkWrap: true,
-                        physics: NeverScrollableScrollPhysics(),
-                        itemCount: items.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          final itemRef = items[index]['itemRef'] as DocumentReference;
-                          return FutureBuilder<DocumentSnapshot>(
-                            future: itemRef.get(),
-                            builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-                              if (snapshot.hasError) {
-                                return Text('Error: ${snapshot.error}');
-                              }
-                              if (!snapshot.hasData) {
-                                return SizedBox();
-                              }
-                              final itemName = snapshot.data!.get('name') as String;
-                              return Text(
-                                '- $itemName',
-                                style: TextStyle(fontSize: 16.0),
-                              );
-                            },
-                          );
-                        },
-                      ),
-                    ],
+                    )
                   );
                 },
               );
@@ -88,6 +111,23 @@ class CompletedOrdersScreen extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+}
+
+class OrderDetailsPage extends StatefulWidget {
+  const OrderDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrderDetailsPage> createState() => _OrderDetailsPageState();
+}
+
+class _OrderDetailsPageState extends State<OrderDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("Text"),
     );
   }
 }
