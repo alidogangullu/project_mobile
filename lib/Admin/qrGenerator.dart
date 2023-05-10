53aa6857d7fb911419c25b9c06deeef98a3edd1a
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2023-05-10 22:34:20 +0300

    exporting QR codes as image files are now possible

--- lib/Admin/qrGenerator.dart
+++ lib/Admin/qrGenerator.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:project_mobile/Admin/menuEdit.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:share_plus/share_plus.dart';
 import '../customWidgets.dart';
 
 class QRHomePage extends StatelessWidget {
@@ -16,7 +20,10 @@ class QRHomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         iconTheme: const IconThemeData(color: Colors.black),
-        title: const Text("Create QR Code", style: TextStyle(color: Colors.black),),
+        title: const Text(
+          "Create QR Code",
+          style: TextStyle(color: Colors.black),
+        ),
         actions: const [
           //todo pdf vb. yöntemler ile qr kodunu dışarı verdirtme
           Icon(Icons.picture_as_pdf),
@@ -49,6 +56,8 @@ class QRHomePage extends StatelessWidget {
                       primary: false,
                       shrinkWrap: true,
                       children: snapshot.data!.docs.map((document) {
+                        final qrCodeData =
+                            "https://restaurantapp-2a43d.web.app/?id=$selectedRestaurantID&tableNo=${document['number']}";
                         //restorant masalarının qr kodlarını listeleme
                         return ClipRRect(
                           borderRadius: const BorderRadius.only(
@@ -56,22 +65,35 @@ class QRHomePage extends StatelessWidget {
                             topRight: Radius.circular(20),
                           ),
                           child: Card(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: QrImage(
-                                      version: QrVersions.auto,
-                                      data:
-                                          //qr kodun içeriği
-                                          "https://restaurantapp-2a43d.web.app/?id=$selectedRestaurantID&tableNo=${document['number']}"),
-                                ),
-                                Text(
-                                  "Table ${document['number']}",
-                                  style: const TextStyle(fontSize: 18,),
-                                ),
-                              ],
+                            child: Padding(
+                              padding: const EdgeInsets.only(bottom: 4.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: QrImage(
+                                        version: QrVersions.auto,
+                                        data: qrCodeData),
+                                  ),
+                                  Text(
+                                    "Table ${document['number']}",
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 25,
+                                    child: ElevatedButton(
+                                      onPressed: () async {
+                                        await _shareQRCode(
+                                            qrCodeData, document['number']);
+                                      },
+                                      child: const Text('Export QR Code'),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         );
@@ -94,8 +116,7 @@ class QRHomePage extends StatelessWidget {
                 child: menuButton(
                   'Save',
                   () async {
-                    int numberOfTables =
-                        int.parse(tableNumberController.text);
+                    int numberOfTables = int.parse(tableNumberController.text);
                     final ref = FirebaseFirestore.instance.collection(
                         "/Restaurants/$selectedRestaurantID/Tables");
 
@@ -128,5 +149,31 @@ class QRHomePage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+Future<void> _shareQRCode(String qrCodeData, int tableNumber) async {
+  try {
+    final qrImageBytes = await QrPainter(
+      data: qrCodeData,
+      version: QrVersions.auto,
+      gapless: false,
+      color: Colors.black,
+      emptyColor: Colors.white,
+    ).toImageData(300.0);
+
+    final tempDir = await getTemporaryDirectory();
+    final tempPath = tempDir.path;
+    final qrImagePath = '$tempPath/qr_code_table_$tableNumber.png';
+
+    final qrImageFile = File(qrImagePath);
+    await qrImageFile.writeAsBytes(qrImageBytes!.buffer.asUint8List());
+
+    await Share.shareFiles([qrImagePath],
+        subject: 'Share QR Code',
+        text: 'Here is the QR code for Table $tableNumber.',
+        mimeTypes: ['image/png']);
+  } catch (e) {
+    print('Failed to share QR code: $e');
   }
 }
