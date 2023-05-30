4593cc2154767a44145cced696fa9c27b9277771
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2023-05-30 19:22:56 +0300

    created global AppColors constants

---
+++ lib/values.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color color50 = Color(0xFFE0F2F2);
+  static const Color color100 = Color(0xFFB3CCCC);
+  static const Color color200 = Color(0xFF80B2B2);
+  static const Color color300 = Color(0xFF4D9999);
+  static const Color color400 = Color(0xFF267F7F);
+  static const Color color500 = Color(0xFF008C8C);
+  static const Color color600 = Color(0xFF007474);
+  static const Color color700 = Color(0xFF006060);
+  static const Color color800 = Color(0xFF004C4C);
+  static const Color color900 = Color(0xFF003838);
+  static const Color white = Color(0xFFFFFFFF);
+}
